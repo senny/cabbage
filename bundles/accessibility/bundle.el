4fe6b29aa64c504d4bc6b408b98c15694f34463c
Yves Senn <yves.senn@garaio.com>
2011-09-17 23:43:41 +0200

    extend the accessibility plugin with essential functionality

--- bundles/accessibility/bundle.el
+++ bundles/accessibility/bundle.el
@@ -1,3 +1,21 @@
+(when window-system
+  (setq frame-title-format '(buffer-file-name "%f" ("%b")))
+  (tooltip-mode -1)
+  (mouse-wheel-mode t))
+
+(transient-mark-mode 1)
+(blink-cursor-mode 1)
+
+(setq use-dialog-box nil
+      visible-bell t
+      echo-keystrokes 0.1
+      inhibit-startup-message t
+      truncate-partial-width-windows nil
+      gnuserv-frame (car (frame-list)))
+
+(setq-default cursor-type '(bar . 2))
+(setq-default indicate-empty-lines t)
+
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 (defun increase-font-size ()
