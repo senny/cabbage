40bd2b3b56cbdd2e6f0b3e5627c39ddc4cb968c4
Yves Senn <yves.senn@gmail.com>
2011-09-18 15:44:11 +0200

    general handling of backup files

--- lib/setup.el
+++ lib/setup.el
@@ -11,3 +11,18 @@
 (setq-default indent-tabs-mode nil)
 
 (setq uniquify-buffer-name-style 'forward)
+
+
+;; global location for emacs backup files
+(defcustom e-max-backup-dir
+  (expand-file-name "~/.emacs.d/backup")
+  "the location where emacs will store auto-save and backup files.")
+
+(make-directory e-max-backup-dir t)
+(setq backup-by-copying t)
+(setq auto-save-list-file-prefix
+      (concat e-max-backup-dir ".auto-saves-"))
+(setq backup-directory-alist
+      `((".*" . ,e-max-backup-dir)))
+(setq auto-save-file-name-transforms
+      `((".*" ,e-max-backup-dir t)))
