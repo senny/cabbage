94d3abda267056d250bbab886efa4c4b25de66b5
Jonas Baumann <jone@jone.ch>
2011-09-18 12:09:41 +0200

    improved install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,49 +1,64 @@
-if [ -d ~/.e-max ]
-then
-  echo "\033[0;33mYou already have e-max installed.\033[0m You'll need to remove ~/.e-max if you want to reinstall"
-  exit
-fi
+#/usr/bin/env sh
 
 if [ -f `dirname $0`/install.sh ]; then
+  # already cloned manually
   if [[ `dirname $0` != /* ]]; then
-    emaxdir=`dirname \`pwd\`/\`dirname $0\` | sed -e 's/\/\.$//'`  # ../
+    # script run with relative path (./scripts/install.sh)
+    if [[ $0 = './install.sh' ]]; then
+      emaxdir=`dirname \`pwd\``
+    else
+      emaxdir=`dirname \`pwd\`/\`dirname $0\` | sed -e 's/\/\.$//'`  # ../
+    fi
+
   else
+    # script run with absolute path (/home/me/e-max/scripts/install.sh)
     emaxdir=`dirname \`dirname $0\``
   fi
 
-  echo "\033[0;34mYou have cloned e-max into $emaxdir, using this one...\033[0m"
+  echo -e "Using already cloned e-max repository at \033[0;32m$emaxdir\033[00m ..."
 
 else
-  emaxdir=`~/.e-max`
-  echo "\033[0;34mCloning e-max to $emaxdir...\033[0m"
+  emaxdir=$HOME/.e-max
+
+  if [ -d $emaxdir ]
+  then
+    echo -e "\033[0;31mYou already have e-max installed.\033[0m You'll need to remove \033[0;31m$emaxdir\033[0m if you want to reinstall"
+    exit 1
+  fi
+
+  echo -e "Cloning e-max to \033[0;32m$emaxdir\033[0m ..."
   /usr/bin/env git clone https://github.com/senny/e-max.git $emaxdir
-  de $emaxdir
+  cd $emaxdir
   /usr/bin/env git submodule init
   /usr/bin/env git submodule update
 fi
 
-echo "\033[0;34mLooking for an existing emacs config...\033[0m"
+echo -e "Looking for an existing emacs config in your home directory..."
 if [ -f ~/.emacs ] || [ -h ~/.emacs ]
 then
-  echo "\033[0;33mFound ~/.emacs.\033[0m \033[0;32]Backing up to ~/.emacs-pre-e-max\033[0m";
-  mv ~/.emacs ~/.emacs-pre-e-max;
+  backupfile=`date "+~$HOME/.emacs-pre-e-max-%Y-%m-%dT%H:%M%:%S"`
+  echo -e "Found \033[0;31m~/.emacs\033[0m. Backing up to \033[0;31m$backupfile\033[0m";
+  mv ~/.emacs $backupfile
 fi
 
 if [ -d ~/.emacs.d ] || [ -h ~/.emacs.d ]
 then
-  echo "\033[0;33mFound ~/.emacs.d.\033[0m \033[0;32]Backing up to ~/.emacs.d-pre-e-max\033[0m";
-  mv ~/.emacs.d ~/.emacs.d-pre-e-max;
+  backupdir=`date "+$HOME/.emacs.d-pre-e-max-%Y%m%d-%H%M%S"`
+  echo -e "Found \033[0;31m~/.emacs.d\033[0m. Backing up to \033[0;31m$backupdir\033[0m";
+  mv ~/.emacs.d $backupdir
 fi
 
-echo "\033[0;34mCreating a ~/.emacs.d directory, containing the emacs load file.\033[0m"
+echo -e "Creating a \033[0;32m~/.emacs.d\033[0m directory, containing the emacs load file."
 mkdir ~/.emacs.d
 
 cat "$emaxdir/templates/init.el" | sed "s:E-MAX-DIR:$emaxdir:g" > ~/.emacs.d/init.el
-# cp $emaxdir/templates/init.el ~/.emacs.d/init.el
 
-echo "\033[0;32m"'  $$$$$$\          $$$$$$\$$$$\   $$$$$$\  $$\   $$\     '"\033[0m"
-echo "\033[0;32m"'  $$  __$$\ $$$$$$\ $$  _$$  _$$\  \____$$\ \$$\ $$  |   '"\033[0m"
-echo "\033[0;32m"'  $$$$$$$$ |\______|$$ / $$ / $$ | $$$$$$$ | \$$$$  /    '"\033[0m"
-echo "\033[0;32m"'  $$   ____|        $$ | $$ | $$ |$$  __$$ | $$  $$<     '"\033[0m"
-echo "\033[0;32m"'  \$$$$$$$\         $$ | $$ | $$ |\$$$$$$$ |$$  /\$$\    '"\033[0m"
-echo "\033[0;32m"'   \_______|        \__| \__| \__| \_______|\__/  \__|   '"\033[0m"
+echo ""
+d="\033[0;34mX\033[0;32m"
+echo -e "\033[0;32m  $d$d$d$d$d$d\          $d$d$d$d$d$d\ $d$d$d$d\   $d$d$d$d$d$d\  $d$d\   $d$d\     \033[0m"
+echo -e "\033[0;32m  $d$d  __$d$d\ $d$d$d$d$d$d\ $d$d  _$d$d  _$d$d\  \____$d$d\  $d$d\ $d$d  |   \033[0m"
+echo -e "\033[0;32m  $d$d$d$d$d$d$d$d |\______|$d$d / $d$d / $d$d | $d$d$d$d$d$d$d |  $d$d$d$d  /    \033[0m"
+echo -e "\033[0;32m  $d$d   ____|        $d$d | $d$d | $d$d |$d$d  __$d$d | $d$d  $d$d<     \033[0m"
+echo -e "\033[0;32m   $d$d$d$d$d$d$d\         $d$d | $d$d | $d$d |\ $d$d$d$d$d$d |$d$d  ^ $d$d\    \033[0m"
+echo -e "\033[0;32m   \_______|        \__| \__| \__| \_______|\__/  \__|   \033[0m"
+echo ""
