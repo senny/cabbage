56cde12edc770fee93c89334f33f6e163b131dff
Yves Senn <yves.senn@garaio.com>
2011-09-19 17:56:47 +0200

    completing-read advice messes up A LOT. close #13

--- bundles/completion/bundle.el
+++ bundles/completion/bundle.el
@@ -13,22 +13,6 @@
 ;;;; Global IDO Completion
 (ido-everywhere t)
 
-;; Replace completing-read wherever possible, unless directed otherwise
-(defadvice completing-read (around use-ido-when-possible activate)
-  "If `ido-everywhere' is t, then use `ido-completing-read' wherever possible.
-Even some places where ido doesn't already enable it."
-  (if (or (not ido-mode)
-          (not ido-everywhere)
-          (boundp 'ido-cur-item)) ; Avoid infinite loop from ido calling completing-read
-      ad-do-it
-    (let ((allcomp (all-completions "" collection predicate)))
-      (if allcomp
-          (setq ad-return-value
-                (ido-completing-read prompt
-                                     allcomp
-                                     nil require-match initial-input hist def))
-        ad-do-it))))
-
 ;;;; AutoComplete
 (require 'auto-complete-config)
 (ac-config-default)
