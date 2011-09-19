cbfe8947f2ed16ef44d8298dfc215f621f05ec4c
Jonas Baumann <jone@jone.ch>
2011-09-19 23:00:52 +0200

    python bundle: pyflakes fixes
    - add a custom for enabling flymake with pyflakes in python-mode
    - make (executable-find "pyflakes") more robust (PATH is loaded later in some cases) and do not cache (makes possible to install pyflakes while running emacs and it works without a restart when reloading buffer)

--- bundles/python/bundle.el
+++ bundles/python/bundle.el
@@ -1,3 +1,15 @@
+;; Configuration
+
+(defcustom e-max-python-pyflakes-enabled
+  t
+  "Enable flymake with pyflakes in python mode"
+  :type 'boolean
+  :group 'e-max)
+
+
+;;;; -------------------------------------
+;;;; Bundle
+
 (defun e-max-python-set-pairs ()
   (e-max--set-pairs '("(" "{" "[" "\"" "\'" "`")))
 
@@ -30,9 +42,10 @@
 (add-hook 'python-mode-hook 'e-max-python-configure-pep8)
 
 
-(when (executable-find "pyflakes")
-  (defun e-max-python-flymake ()
+(defun e-max-python-flymake ()
+  (when (and e-max-python-pyflakes-enabled (executable-find "pyflakes"))
     (when (load "flymake" t)
+      (e-max-flymake-init)
 
       (defun flymake-pyflakes-init ()
         (let* ((temp-file (flymake-init-create-temp-buffer-copy
@@ -45,7 +58,6 @@
       (add-to-list 'flymake-allowed-file-name-masks
                    '("\\.py\\'" flymake-pyflakes-init)))
 
-    (add-hook 'find-file-hook 'flymake-find-file-hook))
+    (add-hook 'find-file-hook 'flymake-find-file-hook)))
 
-  (add-hook 'python-mode-hook 'e-max-python-flymake)
-  (add-hook 'python-mode-hook 'e-max-flymake-init))
+(add-hook 'python-mode-hook 'e-max-python-flymake)
