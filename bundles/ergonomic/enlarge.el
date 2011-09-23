1a8c02de5a0719700ce08a49246cb96ae49704fc
Jonas Baumann <jone@jone.ch>
2011-09-23 18:38:31 +0200

    started working on enlargement stuff

---
+++ bundles/ergonomic/enlarge.el
@@ -0,0 +1,14 @@
+
+(defvar e-max-enlargement--window-configuration-store nil
+  "Hash storing window configurations. persp-name -> list of configs.")
+
+(defun e-max-enlargement--create-winconfig-store-if-nil ()
+  (if (if e-max-enlargement--window-configuration-store nil)
+      (setq e-max-enlargement--window-configuration-store (make-hash-table :test 'equal :size 10))))
+
+(defun e-max-enlargement--get-winconfigs-of-current-persp ()
+  (e-max-enlargement--create-winconfig-store-if-nil)
+
+  (let* ((persp (persp-new persp-curr))
+         (configs (gethash e-max-enlargement--window-configuration-store)))
+    configs))
