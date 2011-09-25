0b0d7ed3a76391b662df05f8079428917aa2c2ca
Jonas Baumann <jone@jone.ch>
2011-09-25 02:45:04 +0200

    various terminal binding fixes & improvements #9

--- bundles/terminal/bundle.el
+++ bundles/terminal/bundle.el
@@ -22,25 +22,47 @@ e-max bindings.")
 
 (when e-max-terminal-fix-keys
   (setq term-bind-key-alist
-        '(("C-c C-c" . term-interrupt-subjob)
-          ("C-f" . isearch-forward)
-          ("M-j" . e-max-term-backward-char)  ;; backward-char
-          ("M-l" . e-max-term-forward-char)  ;; forward -char
+        '(
+          ;; provide some needed functions on other bindings, since they
+          ;; conflict with emacs bindings
+          ("C-c C-c" . term-interrupt-subjob)
+          ("C-c C-d" . term-send-raw)
+          ("C-c <escape>" . e-max-term-escape)
+          ("C-<escape>" . e-max-term-escape)
 
-          ("M-i" . previous-line)  ;; move up - but don't tell terminal
-          ("M-k" . next-line) ;; move down - dont't tell terminal
+          ;; movement
+          ("M-i" . previous-line)  ;; move up - but dont tell terminal
+          ("M-k" . next-line) ;; move down - dont tell terminal
           ("M-C-i" . term-send-up) ;; tell terminal to move up (history)
           ("M-C-k" . term-send-down) ;; tell terminal to move down (history)
 
+          ("M-j" . e-max-term-backward-char)
+          ("M-l" . e-max-term-forward-char)
+          ("M-L" . e-max-term-end-of-line)
+          ("M-C-l" . e-max-term-end-of-line)
+          ("M-J" . e-max-term-beginning-of-line)
+          ("M-C-j" . e-max-term-beginning-of-line)
           ("M-u" . e-max-term-backward-word) ;; backward-word
           ("M-o" . e-max-term-forward-word) ;; forward-word
-          ("M-<backspace>" . term-send-backward-kill-word)
+
+          ;; searching
+          ("C-f" . isearch-forward)
+
+          ;; modifying
+          ("M-d" . term-send-backspace)
           ("M-f" . term-send-del)
+          ("<delete>" . term-send-del)
+          ("M-<backspace>" . term-send-backward-kill-word)
           ("M-v" . term-paste)
           ))
 
   (setq term-unbind-key-list (append term-unbind-key-list
-                                     '("C-p"))))
+                                     '("C-p"
+                                       "C-a"
+                                       "C-w"
+                                       "C-s"
+                                       "C-d"
+                                       ))))
 
 ;; funs
 
@@ -48,14 +70,10 @@ e-max bindings.")
   (interactive)
 
   (e-max-persp "@terminal"
-               (e-max-terminal)))
+               (multi-term-next)))
 
 (e-max-global-set-key (kbd "C-p t") 'e-max-terminal-open-term-persp)
-
-(defun e-max-terminal ()
-  (interactive)
-
-  (multi-term-next))
+(e-max-global-set-key (kbd "C-x t") 'multi-term)
 
 ;; XXX: should be improved, especially for multi-line prompts.
 (defun e-max-terminal--cursor-in-prompt ()
@@ -88,3 +106,19 @@ e-max bindings.")
   (if (e-max-terminal--cursor-in-prompt)
       (term-send-forward-word)
     (forward-word)))
+
+(defun e-max-term-end-of-line ()
+  (interactive)
+  (if (e-max-terminal--cursor-in-prompt)
+      (term-send-raw-string "\C-E")
+    (end-of-line)))
+
+(defun e-max-term-beginning-of-line ()
+  (interactive)
+  (if (e-max-terminal--cursor-in-prompt)
+      (term-send-raw-string "\C-A")
+    (end-of-line)))
+
+(defun e-max-term-escape ()
+  (interactive)
+  (term-send-raw-string "\e033"))
