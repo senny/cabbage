57e6efaeb9b50198b7ba3074fba080282fef9ebd
Yves Senn <yves.senn@gmail.com>
2011-09-26 22:37:55 +0200

    include forward-buffer and backward-buffer to cycle through buffers. #57

--- bundles/power-edit/bundle.el
+++ bundles/power-edit/bundle.el
@@ -33,6 +33,26 @@
   (interactive "*p")
   (move-text-internal arg))
 
+(defun forward-buffer () (interactive)
+  "Opposite of backward-buffer."
+  (let* ((list (reverse (buffer-list)))
+         (buffer (car list)))
+    (while (and (cdr list) (string-match "\\*" (buffer-name buffer)))
+      (progn
+        (setq list (cdr list))
+        (setq buffer (car list))))
+    (switch-to-buffer buffer)))
+
+(defun backward-buffer () (interactive)
+  "Switch to previously selected buffer."
+  (let* ((list (cdr (buffer-list)))
+         (buffer (car list)))
+    (while (and (cdr list) (string-match "\\*" (buffer-name buffer)))
+      (progn
+        (setq list (cdr list))
+        (setq buffer (car list))))
+    (bury-buffer)
+    (switch-to-buffer buffer)))
 
 (e-max-vendor 'textmate)
 (global-set-key (kbd "M-<up>") 'move-text-up)
