21cbbbf3f00644b398e8613da6f855acedde480b
Yves Senn <yves.senn@gmail.com>
2011-10-01 18:23:39 +0200

    cucumber: latest version of feature mode to run cucumber without rake: #69

--- bundles/cucumber/bundle.el
+++ bundles/cucumber/bundle.el
@@ -1,8 +1,11 @@
 (setq feature-default-i18n-file (concat e-max-vendor-dir "feature-mode/i18n.yml"))
+(setq feature-cucumber-command "bundle exec cucumber {feature}")
 
 (e-max-vendor 'feature-mode)
 
 (defun e-max-cucumber-run-single-file (file-name)
+  (when (e-max-bundle-active-p 'ruby)
+    (rvm-activate-corresponding-ruby))
   (feature-run-cucumber '() :feature-file file-name))
 
 (defun e-max-cucumber-hook ()
