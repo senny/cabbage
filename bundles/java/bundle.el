9a2e00af50668ae46a8c5cbd40bb47582464ce21
fred-o <fredrik.appelberg@gmail.com>
2011-11-20 15:29:38 +0100

    Moved the eclim stuff into the completion and java bundles.

---
+++ bundles/java/bundle.el
@@ -0,0 +1,29 @@
+
+(defcustom e-max-java-use-eclim t
+  "Use eclim for managing java files."
+  :type 'boolean
+  :group 'e-max)
+
+(defun e-max-java-init ()
+  "Initialize the java bundle."
+  (when e-max-java-use-eclim
+    (add-to-list 'load-path (concat e-max-vendor-dir "eclim/vendor/"))
+    (e-max-vendor 'eclim)
+
+    (setq eclim-interactive-completion-function 'ido-completing-read)
+
+    (setq help-at-pt-display-when-idle t)
+    (setq help-at-pt-timer-delay 0.1)
+    (help-at-pt-set-timer)
+
+    (global-eclim-mode t)
+
+    (case e-max-completion-framework
+      ('auto-complete
+       (require 'ac-emacs-eclim-source)
+       (add-hook 'eclim-mode-hook (lambda () (add-to-list 'ac-sources 'ac-source-emacs-eclim))))
+      ('company-mode
+       (require 'company-emacs-eclim)
+       (company-emacs-eclim-setup)))))
+
+(e-max-java-init)
