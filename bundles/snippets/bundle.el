2aba12beaad5c759fe9d103a54700d1354df3671
Jonas Baumann <jone@jone.ch>
2011-11-30 08:51:07 +0100

    Snippets bundle: create default snippets directory if it does not exist. refs #88

--- bundles/snippets/bundle.el
+++ bundles/snippets/bundle.el
@@ -7,7 +7,14 @@
 
 (e-max-vendor 'yasnippet-bundle)
 
-(setq yas/root-directory (list "~/.emacs.d/snippets/"
+(defvar e-max-snippets--default-directory
+  (expand-file-name "~/.emacs.d/snippets/")
+  "Default snippets directory.")
+
+(if (not (file-exists-p e-max-snippets--default-directory))
+    (make-directory e-max-snippets--default-directory))
+
+(setq yas/root-directory (list e-max-snippets--default-directory
                                (concat e-max-bundle-dir "snippets/")))
 (yas/initialize)
 (yas/reload-all)
