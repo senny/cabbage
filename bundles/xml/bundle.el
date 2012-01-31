aa56bbd7ce835ee8ff185ff442136c54bc596395
fred-o <fredrik.appelberg@gmail.com>
2012-01-31 22:24:15 +0100

    Added nxml-mode associations for even more file endings. Added automatic closing of tags on '/'. Rebound '>' to automatically insert a closing xml tag (if appropriate)

--- bundles/xml/bundle.el
+++ bundles/xml/bundle.el
@@ -12,15 +12,24 @@
 
 (add-hook 'nxml-completion-hook 'rng-complete nil t)
 (setq rng-nxml-auto-validate-flag t)
-(add-to-list 'auto-mode-alist '("\\.xml$" . nxml-mode))
-(add-to-list 'auto-mode-alist '("\\.tld$" . nxml-mode))
 
+;; automatically enable nxml-mode for various file types
+(add-to-list 'auto-mode-alist
+            (cons (concat "\\." (regexp-opt '("xml" "xsd" "sch" "rng" "xslt" "svg" "rss") t) "\\'")
+                  'nxml-mode))
+
+;; enable nxml-mode when the user starts typing an xml document
+(setq magic-mode-alist
+      (cons '("<\\?xml " . nxml-mode)
+           magic-mode-alist))
+
+;; enable automatic closing of tags
+(setq nxml-slash-auto-complete-flag t)
 
 (defun e-max-xml-set-pairs ()
-  (e-max--set-pairs '("<" "{" "[" "\"" "\'")))
+  (e-max--set-pairs '("{" "[" "\"" "\'")))
 
 (add-hook 'nxml-mode-hook 'e-max-xml-set-pairs)
-
 
 (defun e-max-xml-flymake ()
   (when (and e-max-xml-flymake-enabled (executable-find "xml"))
@@ -30,5 +39,22 @@
       (flymake-find-file-hook))))
 
 (add-hook 'nxml-mode-hook 'e-max-xml-flymake)
+
+;; Rebind '>', so that it automatically inserts a closing xml tag (if
+;; appropriate)
+(defun e-max-nxml-end-tag ()
+  (interactive)
+  (if (eq (face-at-point) 'default)
+      (condition-case nil
+          (nxml-balanced-close-start-tag-inline)
+        (error
+         (insert ">")))
+    (insert ">")))
+
+(add-hook 'nxml-mode-hook
+          (lambda ()
+            ;; rebind > to close the current tag
+            (define-key nxml-mode-map ">" 'e-max-nxml-end-tag)))
+
 
 
