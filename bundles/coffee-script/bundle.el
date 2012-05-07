1f8a8d2c87e58553725a01aed3e3f6bbb3ea7e06
fred-o <fredrik.appelberg@gmail.com>
2012-05-07 10:30:34 +0200

    intelligent activation of cofee-cos-mode

--- bundles/coffee-script/bundle.el
+++ bundles/coffee-script/bundle.el
@@ -2,3 +2,8 @@
 
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
 (add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+
+(add-hook 'coffee-mode-hook '(lambda ()
+                               (and (file-exists-p (buffer-file-name))
+                                    (file-exists-p (coffee-compiled-file-name))
+                                    (coffee-cos-mode t))))
