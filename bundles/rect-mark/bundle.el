72b0f088dc6b7e28bc8348fa309a599f3ed05387
Jonas Baumann <jone@jone.ch>
2012-08-22 13:55:15 +0200

    Rect-mark: add search / replace function binding (C-x r M-r).

--- bundles/rect-mark/bundle.el
+++ bundles/rect-mark/bundle.el
@@ -1,7 +1,36 @@
 (load (concat e-max-vendor-dir "rect-mark"))
 
+(defun rectangle-replace-string (from-string to-string delimited startcol endcol)
+  "Search and replace a rectangle."
+  (interactive
+   (progn (barf-if-buffer-read-only)
+          (let ((common
+                 (query-replace-read-args
+                  (concat "Query replace"
+                          (if current-prefix-arg " word" "")
+                          (if (and transient-mark-mode mark-active) " in region" ""))
+                  nil)))
+            (list (nth 0 common)
+                  (nth 1 common)
+                  (nth 2 common)
+                  (region-beginning)
+                  (region-end)))))
+
+  (goto-char
+   (apply-on-rectangle
+    '(lambda (startcol endcol from-string to-string)
+       (let ((startpoint (progn (move-to-column startcol t)
+                                (point)))
+             (endpoint (progn (move-to-column endcol t)
+                              (point))))
+
+         (replace-string from-string to-string t startpoint endpoint)))
+    startcol endcol from-string to-string)))
+
+
 (e-max-global-set-key (kbd "C-x r M-SPC") 'rm-set-mark)
 (e-max-global-set-key (kbd "C-x r M-x")   'rm-kill-region)
 (e-max-global-set-key (kbd "C-x r M-c")   'rm-kill-ring-save)
+(e-max-global-set-key (kbd "C-x r M-r")   'rectangle-replace-string)
 (e-max-global-set-key (kbd "C-x r s")     'string-rectangle)
 (e-max-global-set-key (kbd "C-x r <down-mouse-1>") 'rm-mouse-drag-region)
