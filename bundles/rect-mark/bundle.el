c0dbe9de327d40ac84fac65adb10314492a68bd3
Jonas Baumann <jone@jone.ch>
2012-08-22 13:54:47 +0200

    Rect-mark: add substitute binding (C-x r s).

--- bundles/rect-mark/bundle.el
+++ bundles/rect-mark/bundle.el
@@ -3,4 +3,5 @@
 (e-max-global-set-key (kbd "C-x r M-SPC") 'rm-set-mark)
 (e-max-global-set-key (kbd "C-x r M-x")   'rm-kill-region)
 (e-max-global-set-key (kbd "C-x r M-c")   'rm-kill-ring-save)
+(e-max-global-set-key (kbd "C-x r s")     'string-rectangle)
 (e-max-global-set-key (kbd "C-x r <down-mouse-1>") 'rm-mouse-drag-region)
