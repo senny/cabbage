f22e062ca58096bcb0257b944e739d770f0a02bd
Samuel Tonini <samuel.tonini@garaio.com>
2013-01-17 10:29:22 +0100

    extend the local.el template file

--- templates/emacs.d/local.el
+++ templates/emacs.d/local.el
@@ -1,3 +1,8 @@
 ;; This config file is the last config file loaded. It is useful for local settings
 ;; such as passwords, which you do not want to share with your emacs installations
 ;; on other machines.
+
+;; Local bundles directory
+;; (add-to-list 'cabbage-bundle-dirs (expand-file-name "~/.emacs.d/bundles/"))
+;; Local vendors directory
+;; (add-to-list 'cabbage-vendor-dirs (expand-file-name "~/.emacs.d/vendor/"))
