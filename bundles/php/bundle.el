a780c604180618cb20ea506c2371dbd8c902f675
Samuel Tonini <tonini.samuel@gmail.com>
2013-03-21 16:34:58 +0100

    add dynamic phpunit command building

--- bundles/php/bundle.el
+++ bundles/php/bundle.el
@@ -1,5 +1,5 @@
 ;;;; -------------------------------------
-;;;; Bundle
+;;;; Php Bundle
 
 ;; load the latest php-mode to get the syntax-higlighting working
 (cabbage-vendor 'php-mode)
@@ -15,11 +15,42 @@
 (defun cabbage-php-mode-hook ()
   (cabbage--set-pairs '("(" "{" "[" "\"" "\'" "|"))
 
-  (when (and buffer-file-name (string-match "Test.php$" buffer-file-name))
-    (setq cabbage-testing-execute-function 'cabbage-phpunit-run-single-file))
+  (defun cabbage-phpunit-first-readable-file (&rest paths)
+    "Returns the first readable file in the list"
+    (dolist (path paths)
+      (when (file-readable-p path)
+        (return path))))
+
+  (defun cabbage-phpunit-command ()
+    "Returns the phpunit command path (vendor/bin or bin/ because of composer)"
+    (let ((path-to-phpunit-command
+           (cabbage-phpunit-first-readable-file (cabbage-project-expand-path "vendor" "bin" "phpunit")
+                                                (cabbage-project-expand-path "bin" "phpunit"))))
+      (if path-to-phpunit-command
+          path-to-phpunit-command
+        "phpunit")))
+
+  (defun cabbage-phpunit-config-file ()
+    "Returns a given phpunit config file if exists (check app/ because of symfony default.)"
+    (let ((path-to-phpunit-config
+           (cabbage-phpunit-first-readable-file (cabbage-project-expand-path "app" "phpunit.xml")
+                                                (cabbage-project-expand-path "phpunit.xml"))))
+      (if path-to-phpunit-config
+          (format "%s %s" "-c" path-to-phpunit-config)
+        nil)))
+
+  (defun cabbage-phpunit-default-options ()
+    (cabbage-phpunit-config-file))
+
+  (defun cabbage-phpunit-runner ()
+    "Returns command line to run phpunit"
+    (format "%s %s" (cabbage-phpunit-command) (cabbage-phpunit-default-options)))
 
   (defun cabbage-phpunit-run-single-file (filename)
-    (compile (format "%s %s" "phpunit" filename))))
+    (compile (format "%s %s" (cabbage-phpunit-runner) filename)))
+
+  (when (and buffer-file-name (string-match "Test.php$" buffer-file-name))
+    (setq cabbage-testing-execute-function 'cabbage-phpunit-run-single-file)))
 
 (when (cabbage-flymake-active-p)
   (cabbage-load-bundle-dependencies "php" '("flymake")))
