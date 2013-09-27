9ddde57f6e764877a5326fd466c3cd895aa24a23
Jonas Baumann <jone@jone.ch>
2013-09-27 15:26:39 +0200

    Update ido-ubiquitous to never newest version. #199

--- bundles/completion/bundle.el
+++ bundles/completion/bundle.el
@@ -63,5 +63,4 @@ function in the chosen completion framework."
 (ido-everywhere t)
 
 (cabbage-vendor 'ido-ubiquitous)
-(add-to-list 'ido-ubiquitous-command-exceptions 'ucs-insert)
 (ido-ubiquitous t)
